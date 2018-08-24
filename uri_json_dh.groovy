e9e189f7870011ae624189a0c2ad65c1554b4739
ericvitale <ericvitale@gmail.com>
2018-08-24 11:09:43 -0400

    Update uri_json_dh.groovy

--- uri_json_dh.groovy
+++ uri_json_dh.groovy
@@ -4,16 +4,26 @@
 * Json Switch Device Handler
 */
 
+public static String version() { return "v0.1.0.20180824" }
+
 include 'asynchttp_v1'
 
 preferences {
     
-	section("Internal Access"){
-		input "internal_ip", "text", title: "Internal IP", required: false
-		input "internal_port", "text", title: "Internal Port (if not 80)", required: false
-		input "json_on", "text", title: "On Json", required: false
-		input "json_off", "text", title: "Off Json", required: false
+	section("Device Settings"){
+		input "internal_ip", "text", title: "Internal IP", required: true
+		input "internal_port", "number", title: "Internal Port", required: true, default: 80, range: "0..65535"
+		input "device_relay", "text", title: "Relay #", required: true, default: "1"
 	}
+    
+    section("Commands"){
+		input "on_command", "text", title: "On Json", required: true, default: "on"
+		input "off_command", "text", title: "Off Json", required: true, default: "off"
+	}
+    
+    section("Logging") {
+        	input "logging", "enum", title: "Log Level", required: false, defaultValue: "INFO", options: ["TRACE", "DEBUG", "INFO", "WARN", "ERROR"]
+	}	
 }
 
 metadata {
@@ -22,25 +32,73 @@ metadata {
 		capability "Switch"		
 	}
 
-	// simulator metadata
-	simulator {
-	}
-
-	// UI tile definitions
 	tiles {
 		standardTile("button", "device.switch", width: 2, height: 2, canChangeIcon: true) {
 			state "off", label: 'Off', action: "switch.on", icon: "st.switches.switch.off", backgroundColor: "#ffffff", nextState: "on"
-				state "on", label: 'On', action: "switch.off", icon: "st.switches.switch.on", backgroundColor: "#79b821", nextState: "off"
+				state "on", label: 'On', action: "switch.off", icon: "st.switches.switch.on", backgroundColor: "#00a0dc", nextState: "off"
 		}
 		standardTile("offButton", "device.switch", width: 1, height: 1, canChangeIcon: true) {
 			state "default", label: 'Force Off', action: "switch.off", icon: "st.switches.switch.off", backgroundColor: "#ffffff"
 		}
 		standardTile("onButton", "device.switch", width: 1, height: 1, canChangeIcon: true) {
-			state "default", label: 'Force On', action: "switch.on", icon: "st.switches.switch.on", backgroundColor: "#79b821"
+			state "default", label: 'Force On', action: "switch.on", icon: "st.switches.switch.on", backgroundColor: "#00a0dc"
 		}
 		main "button"
 			details (["button","onButton","offButton"])
 	}
+}
+
+def updated() {
+	setIpAddress(internal_ip)
+    log("IP Address set to ${getIpAddress()}.", "INFO")
+    setPort(internal_port)
+    log("Port set to ${getPort()}.", "INFO")
+	setRelay(device_relay)
+    log("Relay set to ${getRelay()}.", "INFO")
+    setOnCommand(on_command)
+    log("On command set to ${getOnCommand()}.", "INFO")
+    setOffCommand(off_command)
+    log("Off command set to ${getOffCommand()}.", "INFO")
+}
+
+def setIpAddress(value) {
+	state.deviceIP = value
+}
+
+def getIpAddress() {
+	return state.deviceIP
+}
+
+def setPort(value) {
+	state.thePort = value
+}
+
+def getPort() {
+	return state.thePort
+}
+
+def setRelay(value) {
+	state.theRelay = value
+}
+
+def getRelay() {
+	return state.theRelay
+}
+
+def setOnCommand(value) {
+	state.onCommand = value
+}
+
+def getOnCommand() {
+	return state.onCommand
+}
+
+def setOffCommand(value) {
+	state.offCommand = value
+}
+
+def getOffCommand() {
+	return state.offCommand
 }
 
 def parse(String description) {
@@ -48,51 +106,30 @@ def parse(String description) {
 }
 
 def on() {
-		def port
-			if (internal_port){
-				port = "${internal_port}"
-			} else {
-				port = 80
-			}
-
-		def commands =  ["relay1":"on", "relay2":"on", "relay3":"on", "relay4":"on"]
-	
-		def params =    [
-				uri: "${internal_ip}:${port}",
-				headers: ["Content-Type": "application/json", "Accept": "application/json"],
-				body: commands,
-				]
-			
-		//sendHubCommand(result)
-		sendEvent(name: "switch", value: "on") 
-		log.debug "Executing ON" 
-		log.debug result
-		
-		asynchttp_v1.post('postResponseHandler', params)
+		sendCommand(getOnCommand())
+        sendEvent(name: "switch", value: "on") 
+		log("Sending ${getOnCommand()} as part of the on event.", "INFO")
 }
 
 def off() {
-		def port
-			if (internal_port){
-				port = "${internal_port}"
-			} else {
-				port = 80
-			}
+		sendCommand(getOffCommand())
+		sendEvent(name: "switch", value: "off")
+		log("Sending ${getOffCommand()} as part of the off event.", "INFO")
+}
 
-		def commands =  ["relay1":"on", "relay2":"on", "relay3":"on", "relay4":"on"]
+def sendCommand(theCommandValue) {
+	def commands =  ["${getRelay()}": "${theCommandValue}"]
 	
-		def params =    [
-				uri: "${internal_ip}:${port}",
-				headers: ["Content-Type": "application/json", "Accept": "application/json"],
-				body: commands,
-				]
-
-			//sendHubCommand(result)
-			sendEvent(name: "switch", value: "off")
-			log.debug "Executing OFF" 
-			log.debug result
+	def params = [
+				 	uri: 'http://api.github.com',
+					headers: [	"Content-Type":"application/json", 
+        		  				"Accept":"application/json"],
+				  	body: commands
+				 ]
+    
+    log("Params = ${params}", "DEBUG")
 		
-		asynchttp_v1.post('postResponseHandler', params)
+	asynchttp_v1.post('postResponseHandler', params)
 }
 
 def postResponseHandler(response, data) {
@@ -101,5 +138,57 @@ def postResponseHandler(response, data) {
 	log.info "POST response received from the device."
     } else {
         log.error "POST Error: ${response.getErrorData()}"
+    }
+}
+
+private getLogPrefix() {
+	return "uri_switch.${version()}.${device.label}>>>"
+}
+
+private determineLogLevel(data) {
+    switch (data?.toUpperCase()) {
+        case "TRACE":
+            return 0
+            break
+        case "DEBUG":
+            return 1
+            break
+        case "INFO":
+            return 2
+            break
+        case "WARN":
+            return 3
+            break
+        case "ERROR":
+        	return 4
+            break
+        default:
+            return 1
+    }
+}
+
+def log(data, type) {
+    data = "${getLogPrefix()} ${data ?: ''}"
+        
+    if (determineLogLevel(type) >= determineLogLevel(settings?.logging ?: "INFO")) {
+        switch (type?.toUpperCase()) {
+            case "TRACE":
+                log.trace "${data}"
+                break
+            case "DEBUG":
+                log.debug "${data}"
+                break
+            case "INFO":
+                log.info "${data}"
+                break
+            case "WARN":
+                log.warn "${data}"
+                break
+            case "ERROR":
+                log.error "${data}"
+                break
+            default:
+                log.error "${getLogPrefix()} Invalid Log Setting"
+        }
     }
 }
